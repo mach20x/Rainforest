9b9529df5b6707817085053ded42be9ad724f898
Raul <raulmartinez1855@gmail.com>
2017-10-20 19:33:24 -0400

    added db to app

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171020212419) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -22,10 +25,11 @@ ActiveRecord::Schema.define(version: 20171020212419) do
 
   create_table "reviews", force: :cascade do |t|
     t.text "comment"
-    t.integer "product_id"
+    t.bigint "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_reviews_on_product_id"
   end
 
+  add_foreign_key "reviews", "products"
 end
